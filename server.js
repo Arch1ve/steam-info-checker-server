a8b6726b79b6646cedc4a62fa984a12f0b4be691
aavostr <aavostr@gmail.com>
2024-06-19 21:48:47 +0300

    connect databse

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Client } = require('pg');
+
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Настройка подключения к базе данных
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const config = {
+  connectionString:
+      "postgres://cloud_user:Temikadza_2002@gobijita.beget.app:5432/default_db"
+};
+
+const database = new Client(config);
+
+database.connect((err) => {
+  if (err) throw err;
 });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -21,7 +25,7 @@ app.get('/', (req, res) => {
 // Пример запроса к базе данных
 app.get('/users', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await database.query('SELECT * FROM users');
 
     res.json(result.rows);
   } catch (err) {
@@ -32,7 +36,7 @@ app.get('/users', async (req, res) => {
 
 app.get('/trade_items', async (req, res) => {
   try {
-    const result = await pool.query('SELECT users.username, trade_items.item_link, trade_items.price, trade_items.quantity FROM users JOIN trade_items ON users.id = trade_items.user_id;');
+    const result = await database.query('SELECT users.username, trade_items.item_link, trade_items.price, trade_items.quantity FROM users JOIN trade_items ON users.id = trade_items.user_id;');
     
     res.json(result.rows);
   } catch (err) {
